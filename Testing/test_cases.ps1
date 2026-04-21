38ab8da3ff28886855f14a488e4a7bd7b4b2ad5b
abaubinas <abaubinas@microsoft.com>
2026-04-21 16:20:37 +0200

    test

--- Testing/test_cases.ps1
+++ Testing/test_cases.ps1
@@ -24,3 +24,26 @@
 .\process-workspaces.ps1
 #3 upload changes to eventstream
 .\update-eventstream.ps1 -WorkspaceId "611585cb-6332-4849-995e-efce839973f1" -EventstreamId "595ac356-b237-422f-a5d4-6c85146a3897"
+
+
+find_replace:
+  - find_value: "611585cb-6332-4849-995e-efce839973f1" #Workspace GUID to find
+    replace_value:
+      DEV: "dfe401d5-41e3-4ad9-8e82-a3886d070f3f"
+      TEST: "dfe401d5-41e3-4ad9-8e82-a3886d070f3f"
+      PROD: "dfe401d5-41e3-4ad9-8e82-a3886d070f3f"
+    item_type: ["Notebook"]
+
+  - find_value: "0160fd4d-7e00-4c5e-8995-4a9174b13b63" #Lakehouse AopConfigLH GUID to find
+    replace_value:
+      DEV: "07e72d95-7550-4b18-a9d2-52e2e69b4830"
+      TEST: "07e72d95-7550-4b18-a9d2-52e2e69b4830"
+      PROD: "07e72d95-7550-4b18-a9d2-52e2e69b4830" 
+    item_type: ["Notebook"]
+
+  - find_value: "https://trd-6uegjpfbf030eemxtw.z1.kusto.fabric.microsoft.com" #kusto cluster uri to find
+    replace_value:
+      DEV: "https://trd-6uegjpfbf030eemxtw.z1.kusto.fabric.microsoft.com"
+      TEST: "https://trd-6uegjpfbf030eemxtw.z1.kusto.fabric.microsoft.com"
+      PROD: "https://trd-6uegjpfbf030eemxtw.z1.kusto.fabric.microsoft.com"
+    item_type: ["Notebook"]
